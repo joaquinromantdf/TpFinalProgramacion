5791df88ee184c6454fbb256b0edc25461e3a88f
joaquin <joaquinroman2006@gmail.com>
2024-07-03 12:17:30 -0300

    codigo del componente 'GestionPersonas' terminado

---
+++ src/componentes/GestionPersonas.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import ListaPersonas from "./ListaPersonas";
+import AgregarPersona from "./AgregarPersona";
+
+export default class GestionPersonas extends Component {
+  state = {
+    personas: []
+  };
+
+  onPersonaAgregada = (persona) => {
+    this.setState((prevState) => ({
+      personas: [...prevState.personas, persona]
+    }));
+  };
+
+  render() {
+    const { token } = this.props;
+
+    return (
+      <div>
+        <h2>Gestionar Personas</h2>
+        <AgregarPersona token={token} onPersonaAgregada={this.onPersonaAgregada} />
+        <ListaPersonas token={token} personas={this.state.personas} />
+      </div>
+    );
+  }
+}
