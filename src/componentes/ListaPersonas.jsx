b6d0f6f63975a07bea18d905fd98ac0c68048749
joaquin <joaquinroman2006@gmail.com>
2024-07-03 12:18:30 -0300

    codigo del componente 'ListaPersonas' terminado

---
+++ src/componentes/ListaPersonas.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class ListaPersonas extends Component {
+  render() {
+    const { personas } = this.props;
+
+    if (!personas || personas.length === 0) {
+      return <p>No hay personas registradas.</p>;
+    }
+
+    return (
+      <div>
+        <h2>Lista de Personas</h2>
+        <ul>
+          {personas.map((persona, index) => (
+            <li key={persona.persona_id || persona.id || index}>
+              {persona && persona.nombres ? 
+                `${persona.nombres} ${persona.apellidos} - ${persona.documento}` : 
+                'Datos incompletos'}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
