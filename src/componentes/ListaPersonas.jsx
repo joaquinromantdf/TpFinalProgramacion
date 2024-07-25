1e7ea09fb74aa99236dcb2f234e735b66ec712f3
joaquin <joaquinroman2006@gmail.com>
2024-07-25 20:05:52 -0300

    Asegurar de que el componente ListaPersonas está recibiendo y renderizando correctamente los datos de las personas

--- src/componentes/ListaPersonas.jsx
+++ src/componentes/ListaPersonas.jsx
@@ -6,22 +6,22 @@ export default class ListaPersonas extends Component {
   };
   render() {
     const { personas } = this.props;
-
+  
     if (!Array.isArray(personas) || personas.length === 0) {
       return <p>No hay personas registradas.</p>;
     }
-
+  
     return (
       <div>
         <h2>Lista de Personas</h2>
         <ul>
           {personas.map((persona, index) => (
             <li key={persona.id || index}>
-              {persona.nombres}
+              {persona.nombres} {persona.apellidos}
             </li>
           ))}
         </ul>
       </div>
     );
-  }
+  }  
 }
